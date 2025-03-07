8ca6731ef45f31aa49d64ea854ec28bef6c4bbf5
ram <annatram050604@gmail.com>
2025-03-08 00:37:19 +0530

    first commit

---
+++ src/main/java/in/arc/ArcDemoApplication.java
@@ -0,0 +1,13 @@
+package in.arc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArcDemoApplication.class, args);
+	}
+
+}
